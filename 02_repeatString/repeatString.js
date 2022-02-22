c8b2a033fd59ba7092468b6ccd52d3bcea5f8e38
Ahmed BEN JMII <QN6334@engie.com>
2022-02-22 17:22:57 +0100

    make all tests of function repeat string succeed

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,8 +1,12 @@
 const repeatString = function(word, n_times) {
 	let result = '';
+	if (n_times>=0){
 	for (let i = 0; i<n_times; i++)
 		result = result+word;
-	return result;
+	}
+	else
+		result = 'ERROR';
+		return result;
 };
 
 // Do not edit below this line
