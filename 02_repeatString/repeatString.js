99dd004c249387f11279b99b491c441d472d1757
Ahmed BEN JMII <QN6334@engie.com>
2022-02-22 17:14:18 +0100

    make function repeat string

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,8 @@
-const repeatString = function() {
-
+const repeatString = function(word, n_times) {
+	let result = '';
+	for (let i = 0; i<n_times; i++)
+		result = result+word;
+	return result;
 };
 
 // Do not edit below this line
