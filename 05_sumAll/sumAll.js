62ec28a9b697848044511389d9f88fb1acf861a6
Ahmed BEN JMII <QN6334@engie.com>
2022-02-23 17:34:23 +0100

    sumAll exercise

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,20 @@
-const sumAll = function() {
-
+const sumAll = function(n, m) {
+	if ( !Number.isFinite(n) || !Number.isFinite(m) || (n < 0) || (m < 0))
+		return 'ERROR';
+	else {
+		let max = n, min = m, result = 0;
+		if (n < m) {
+			max = m;
+			min = n;
+		}
+		while (max >= min) {
+			result = result + max ;
+			max = max -1;
+		}
+	return result;
+	}
+	
+	
 };
 
 // Do not edit below this line
