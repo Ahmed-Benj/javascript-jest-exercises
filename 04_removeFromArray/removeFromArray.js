6c383d7eef6853708e4aafc63c842ab7945247bd
Ahmed BEN JMII <QN6334@engie.com>
2022-02-23 15:56:48 +0100

    removeFromArray exercise

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,9 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(array) {
+	let args = Array.prototype.slice.call(arguments, 1);
+	for (const element of args) {
+		array = array.filter(el => el !== element)
+	}
+    return(array);
 };
 
 // Do not edit below this line
