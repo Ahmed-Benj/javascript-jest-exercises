17bdaca432813c01d2fe46f5df69af0b9f65aae9
Ahmed BEN JMII <QN6334@engie.com>
2022-02-25 11:20:10 +0100

    make 08_calculator

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,30 @@
-const add = function() {
-	
+const add = function(a,b) {
+	return a+b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a,b) {
+	return a-b;
 };
 
-const sum = function() {
-	
+const sum = function(ar) {
+	return ar.reduce((a, b) => a + b, 0);
 };
 
-const multiply = function() {
-
+const multiply = function(ar) {
+	return ar.reduce((a, b) => a * b, 1);
 };
 
-const power = function() {
-	
+const power = function(a,b) {
+	return Math.pow(a,b);
 };
 
-const factorial = function() {
-	
+const factorial = function(a) {
+	let result = 1;
+	while(a > 0){
+		result = a * result;
+		a = a - 1;
+	}
+	return result;
 };
 
 // Do not edit below this line
