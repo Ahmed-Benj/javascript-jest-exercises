d2d65d7d3384589873a89089b71ae7513d011af0
Ahmed BEN JMII <QN6334@engie.com>
2022-02-25 12:04:18 +0100

    make 09_palindromes

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,11 @@
-const palindromes = function () {
-
+const palindromes = function (inStr) {
+	let innStr = inStr.replace(/[.,\/#!$%\^&\*;:{}=\-_`~()]/g,"");
+	innStr = innStr.replace(/\s{1,}/g,"");
+	innStr= innStr.toLowerCase();
+	return (innStr == innStr.split('').reverse().join(''));
 };
 
 // Do not edit below this line
 module.exports = palindromes;
+
+
