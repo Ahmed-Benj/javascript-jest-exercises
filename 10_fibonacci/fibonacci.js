317938bc1644485438aad827a56a09d55507a83b
Ahmed BEN JMII <QN6334@engie.com>
2022-02-25 19:33:38 +0100

    make 10_fibonacci

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,12 @@
-const fibonacci = function() {
-
+const fibonacci = function(a) {
+	if (a<0)
+		return "OOPS";
+	let arrFib = [1,1];
+	for (let i = 0; i < a ; i++){
+		arrFib.push(arrFib[i]+arrFib[i+1]);
+	}
+	console.log(arrFib);
+	return arrFib[a-1];
 };
 
 // Do not edit below this line
