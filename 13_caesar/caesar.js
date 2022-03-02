6fa5706ca6c839f6c2586089c926739c0c2ee046
Ahmed BEN JMII <QN6334@engie.com>
2022-03-02 18:55:09 +0100

    begin caesar

--- 13_caesar/caesar.js
+++ 13_caesar/caesar.js
@@ -1,5 +1,15 @@
-const caesar = function() {
-
+const caesar = function(word,number) {
+	let result = '';
+	for (var i = 0; i < word.length; i++) {
+		//console.log(word.charAt(i));
+		//console.log(!!(word.charAt(i)).match(/[.,:!?]/g));
+		if (!(word.charAt(i)).match(/[.,:!? ]/))
+			result = result + String.fromCharCode(word.charCodeAt(i)+number);
+		else
+			result = result + word.charAt(i);
+	}
+	
+	return(result);
 };
 
 // Do not edit below this line
