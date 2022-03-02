de3ca439c90275c3c279e46dee35055a594223b3
Ahmed BEN JMII <QN6334@engie.com>
2022-03-02 12:00:49 +0100

    make findTheOldest

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,4 +1,18 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+	let max = 0,current,year, maxPerson;
+	let result = people.map( person => {
+		if (person.yearOfDeath == undefined){
+			year = new Date().getFullYear();
+			currentPersonAge = year - person.yearOfBirth;
+		}
+		else
+			currentPersonAge = person.yearOfDeath - person.yearOfBirth;
+		if (currentPersonAge> max){
+			max = currentPersonAge;
+			maxPerson = person;
+		}
+	});
+	return(maxPerson);
 
 };
 
